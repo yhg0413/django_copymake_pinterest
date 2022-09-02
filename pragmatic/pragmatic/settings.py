f3162e9a9247080e681d305d43934258f93e0f4d
yhg0413 <yanghuigeon9@gmail.com>
2022-09-02 17:24:59 +0900

    부트스트렙 적용중

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'bootstrap4',
+    'accountapp',
 ]
 
 MIDDLEWARE = [
