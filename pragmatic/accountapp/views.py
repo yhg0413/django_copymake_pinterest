12d58bddff2b6a793977afe58767ebab740e3b70
yhg0413 <yanghuigeon9@gmail.com>
2022-09-02 17:00:34 +0900

    sign up create view add

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy, reverse
+
 # Create your views here.
 
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')
+
+
+
+# Create your models here.
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
