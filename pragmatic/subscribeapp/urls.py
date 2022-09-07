5325780d68fd1f17e311adbcaa985fec3a8027e4
yhg0413 <yanghuigeon9@gmail.com>
2022-09-07 17:20:12 +0900

    wysiiwyg add, article create, update fix

--- pragmatic/subscribeapp/urls.py
+++ pragmatic/subscribeapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubcriptionListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
-    path('subscribe/', SubscriptionView.as_view(), name='subscribe')
+    path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
+    path('list/',SubcriptionListView.as_view(), name='list'),
 ]
